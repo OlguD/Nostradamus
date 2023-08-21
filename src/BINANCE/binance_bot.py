28d921fd47cd80fa828da3e028a3f86ea54c9a02
OlguD <olgudegirmenci34@gmail.com>
2023-08-21 18:33:27 +0300

    telegram-bot-created, binance package installed

---
+++ src/BINANCE/binance_bot.py
@@ -0,0 +1,16 @@
+from binance.client import Client
+from config import API_KEY, API_SECRET  # API anahtarlarınızı buradan alın
+
+# Binance API istemcisini oluşturun
+client = Client(API_KEY, API_SECRET)
+
+# Örnek olarak bakiye bilgisini alalım
+account_info = client.get_account()
+balances = account_info['balances']
+
+# Hesap bakiyesini gösterelim
+for balance in balances:
+    asset = balance['asset']
+    free = balance['free']
+    locked = balance['locked']
+    print(f"{asset}: Free: {free}, Locked: {locked}")
